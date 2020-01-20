a2492a3243374a920cc4d0d39a71eea618ad2567
asosunoff <al.sosunoff@gmail.com>
2020-01-20 21:15:46 +0300

    update

---
+++ compile.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+function getDirectoryName {
+	echo $(find -mindepth 1 -maxdepth 1 -type d -regex '^\.\/[^.].*');
+}
+
+>README.md
+
+for fileMain in $(getDirectoryName); do
+	
+	folderName="${fileMain:2}";
+
+	# echo "# $folderName"
+
+	echo ""
+
+	for fileSub in $(find ./$folderName -name '*.sh'); do		
+		IFS=$'\n'
+
+		is_header=true
+		
+		for line in $(cat $fileSub); do
+			if $is_header; then
+				echo ${line}
+				is_header=false
+				echo "\`\`\`bash"
+			else
+				echo "$line"
+			fi
+		done
+		echo "\`\`\`"
+		echo ""
+	done
+done >>README.md
