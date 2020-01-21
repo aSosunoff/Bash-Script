9d42941ee6db9240a83250fc3f0a3e471fa22b41
aSosunoff <al.sosunoff@gmail.com>
2020-01-21 09:19:01 +0300

    update

--- compile.sh
+++ compile.sh
@@ -4,32 +4,51 @@ function getDirectoryName {
 	echo $(find -mindepth 1 -maxdepth 1 -type d -regex '^\.\/[^.].*');
 }
 
+function getHeader {
+	if [ -n $1 ]; then
+		fileSub=$1
+	
+		count_line=0
+		IFS=$'\n'
+		
+		for line in $(cat $fileSub); do
+			count_line=$[ $count_line + 1 ]
+
+			if [ ":<<////" = "${line:0:-1}" ]; then
+				echo $(sed -n "$[ $count_line + 1 ]{p;q}" $fileSub)
+				exit
+			fi
+		done
+	fi
+	echo ''
+}
+
 >README.md
 
 for fileMain in $(getDirectoryName); do
-	
 	folderName="${fileMain:2}";
-
-	# echo "# $folderName"
-
 	echo ""
 
-	for fileSub in $(find ./$folderName -name '*.sh'); do		
+	for fileSub in $(find ./$folderName -name '*.sh'); do
 		IFS=$'\n'
-
-		is_header=true
 		
+		echo "# $(getHeader $fileSub)"
+		echo ""
+		echo '```bash'
+		closer='```'
+
 		for line in $(cat $fileSub); do
-			if $is_header; then
-				echo ${line}
-				echo ""
-				is_header=false
-				echo "\`\`\`bash"
-			else
+			if [ ":<<////" = "${line:0:-1}" ]; then
+				echo "$closer"
+				echo "<details>"
+				echo "	<summary>Детальная информация</summary>"
+				closer="</details>"
+			elif [ "////" != "$line" ]; then
 				echo "$line"
 			fi
 		done
-		echo "\`\`\`"
+
+		echo "$closer"
 		echo ""
 	done
 done >>README.md
