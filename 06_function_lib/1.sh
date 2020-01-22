6ac887391c7e161c7bb045a87c43d8f49518d4bc
asosunoff <al.sosunoff@gmail.com>
2020-01-22 21:29:23 +0300

    update

--- 06_function_lib/1.sh
+++ 06_function_lib/1.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-function functionTest {
-	echo "$1" # arguments are accessible through FunctionTest, echo "$1" # arguments are accessible through FunctionTest, ,...,...
-	echo "$2" # arguments are accessible through FunctionTest, echo "$1" # arguments are accessible through FunctionTest, ,...,...
-	echo '1 + 1'
-	return $[1 + 1]
-}
+# Функцию можно объявить так:
 
-functionTest 1 2
-echo "return value from functionTest is => $?"
+# functionName {
+# }
+
+# Или так:
+
+# functionName() {
+# }
+
+# Функцию можно вызвать без аргументов и с аргументами.
+:<<////
+Объявление функций
+////
