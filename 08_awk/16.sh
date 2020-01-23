6e3a1b4b9de6d79c6372f80a46b79772858a071a
asosunoff <al.sosunoff@gmail.com>
2020-01-23 21:36:59 +0300

    update

---
+++ 08_awk/16.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+awk '
+function myprint(){
+    print $1,$2
+}
+BEGIN{FS="."}
+{myprint()}' myfile_2
+:<<////
+Пользовательские функции
+
+При необходимости вы можете создавать собственные функции `awk`. Такие функции можно использовать так же, как встроенные:
+
+```bash
+$ awk '
+    function myprint() {
+        printf "The user %s has home path at %s\n", $1,$6
+    }
+    BEGIN{FS=":"}
+    {
+        myprint()
+    }' /etc/passwd
+```
+
+В примере используется заданная нами функция myprint, которая выводит данные.
+////
